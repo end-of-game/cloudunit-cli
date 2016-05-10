cf33848d4e7fe32cc6c4289e78cd87d7bb5c0dda
nicolas <zepouet@gmail.com>
2016-05-10 10:29:10 +0200

    Add new Field for display name (diffenrence between technical name et display name). + PrefixEnv

--- src/main/java/fr/treeptik/cloudunit/cli/model/Image.java
+++ src/main/java/fr/treeptik/cloudunit/cli/model/Image.java
@@ -35,6 +35,24 @@ public class Image implements Serializable {
     private String status;
     private String imageType;
     private String managerName;
+    private String displayName;
+    private String prefixEnv;
+
+    public String getPrefixEnv() {
+        return prefixEnv;
+    }
+
+    public void setPrefixEnv(String prefixEnv) {
+        this.prefixEnv = prefixEnv;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
 
     @JsonIgnore
     private List<Module> modules;
